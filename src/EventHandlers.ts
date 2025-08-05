c9c8e5f22f5bfa7efd442e166c93eef392e4d2a2
Marsot Pierre <pierre.mmarsot@gmail.com>
2025-08-05 19:55:46 +0200

    fix events

--- src/EventHandlers.ts
+++ src/EventHandlers.ts
@@ -1,35 +1,35 @@
-import { ERC20 } from "generated";
+import { MyERC20 } from "generated";
 import axios from "axios";
 
 const ZERO_ADDRESS = "0x0000000000000000000000000000000000000000";
 const CURVE_LOCKER = "0x52f541764E6e90eeBc5c21Ff570De0e2D63766B6";
 
-ERC20.Transfer.handler(
+MyERC20.Transfer.handler(
   async ({ event, context }: { event: any, context: any }) => {
-    context.Transfer.set({
-      id: `${event.chainId}_${event.transaction.hash}_${event.block.number}_${event.logIndex}`,
-      product: "curve",
-      tx_type: "deposit",
-      lp: event.srcAddress,
-      amount: event.params.value,
-      price: await getLpPrice(event.srcAddress, "ethereum", event.block.timestamp)
-    });
-  },
-  { wildcard: true, eventFilters: { from: ZERO_ADDRESS, to: CURVE_LOCKER } }
-);
+    
+    let tx_type = "";
+    if(event.params.from.toLowerCase() === ZERO_ADDRESS.toLowerCase()) {
+      tx_type = "deposit";
+    } else {
+      tx_type = "withdraw";
+    }
 
-ERC20.Transfer.handler(
-  async ({ event, context }: { event: any, context: any }) => {
     context.Transfer.set({
-      id: `${event.chainId}_${event.transaction.hash}_${event.block.number}_${event.logIndex}`,
+      id: `${tx_type}_${event.chainId}_${event.transaction.hash}_${event.block.number}_${event.logIndex}`,
       product: "curve",
-      tx_type: "withdraw",
+      tx_type,
       lp: event.srcAddress,
       amount: event.params.value,
       price: await getLpPrice(event.srcAddress, "ethereum", event.block.timestamp)
     });
   },
-  { wildcard: true, eventFilters: { from: CURVE_LOCKER, to: ZERO_ADDRESS } }
+  { 
+    wildcard: true, 
+    eventFilters: [
+      { from: ZERO_ADDRESS, to: CURVE_LOCKER },
+      { from: CURVE_LOCKER, to: ZERO_ADDRESS },
+    ] 
+  }
 );
 
 const getLpPrice = async (lp: string, chain: string, timestamp: number): Promise<number> => {
